703d21afd564848aaa3a0ea3e44b29a2fe3bc79a
anoChick <anoinchick@gmail.com>
2016-06-27 02:35:45 +0900

    circleの実験1

--- spec/features/top_spec.rb
+++ spec/features/top_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.feature 'top', type: :feature do
   scenario 'User creates a new widget' do
     visit '/'
-    page.save_screenshot('screenshot.png')
+    p ENV
+    p ENV['CIRCLE_BUILD_NUM']
+    p page.save_screenshot('screenshot.png')
     expect(page).to have_text("トップページ")
   end
 end
