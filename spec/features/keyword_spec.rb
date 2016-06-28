3c5c4441ca4850997cfa83155cae97dec7b20efd
Dueno <anoinchick@gmail.com>
2016-06-29 04:16:36 +0900

    specのURIえんこーど

--- spec/features/keyword_spec.rb
+++ spec/features/keyword_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 require 'aws-sdk'
 RSpec.feature 'keyword', type: :feature do
   scenario 'キーワード画面が表示される' do
-    visit "/keyword?q=酸欠少女"
+    visit URI.encode "/keyword?q=酸欠少女"
     expect(page).to have_text("酸欠少女")
     upload_evidence_image('ID0002')
   end
