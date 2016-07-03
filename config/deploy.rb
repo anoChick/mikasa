3e871748fa44fb4fee8ebc19ba5c09c4c156e5ff
anoChick <anoinchick@gmail.com>
2016-07-04 01:10:17 +0900

    settingsを抜く

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,9 +23,7 @@ set :deploy_to, '/var/www/rails/mikasa'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/settings/production.yml')
-
-# Default value for linked_dirs is []
+set :linked_files, fetch(:linked_files, []).push('.env')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
